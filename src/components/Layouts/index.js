838493dd295deae9481b6eaa8f361d830a27b9c3
le1205 <legend.dev.goD@outlook.com>
2023-03-27 14:22:15 -0400

    Add backgound image in Layout component.

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -6,11 +6,25 @@ import Box from '@mui/material/Box'
 // custom components
 import Navigation from 'src/components/Navigation'
 
+// assets
+import Background from 'src/assets/images/background.png'
+
 const Layout = ({ children }) => {
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'noWrap' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'noWrap',
+        overflow: 'hidden',
+      }}
+    >
       <Navigation />
-      <Box sx={{ flexGrow: 1 }}>{children}</Box>
+      <Box sx={{ flexGrow: 1, position: 'relative', maxHeight: '100vh' }}>
+        <Box sx={{ position: 'absolute', inset: 0 }}>
+          <img src={Background} alt="" style={{ width: '100%', height: '100vh', objectFit: 'cover' }} />
+        </Box>
+        {children}
+      </Box>
     </Box>
   )
 }
